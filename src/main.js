11bf936177eeb9a0c504f5028c946b0a10dd9deb
Moe <18525355@qq.com>
2023-01-05 14:55:11 +0800

    修改键盘事件冒泡

--- src/main.js
+++ src/main.js
@@ -58,19 +58,19 @@ window.onbeforeunload = () => {
   localStorage.setItem('x', string)
 }
 
-
+  //输入框键盘事件,阻止冒泡
+  $('input').on('keypress', (e) => {
+    e.stopPropagation();
+});
 
 //键盘事件：跳转对应的网页
 $(document).on('keypress', (e) => {
-  const {key} = e
-  for (let i = 0; i < hashMap.length; i++) {
-    if (hashMap[i].logo.toLowerCase() === key) {
-      window.open(hashMap[i].url)
+    const {key} = e
+    for (let i = 0; i < hashMap.length; i++) {
+      if (hashMap[i].logo.toLowerCase() === key) {
+        window.open(hashMap[i].url)
+      }
     }
-  }
-});
+  });
 
-//输入框键盘事件,阻止冒泡
-$input.on('keypress', (e) => {
-    e.stopPropagation();
-});
+  
